1131f38c91f53a7860020bc771eae84928416640
GabrielRehbein <gabrielrehbei@gmail.com>
2025-03-15 21:06:55 -0300

    adicionando enviroments

--- core/settings.py
+++ core/settings.py
@@ -82,10 +82,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'picpay_db',
-        'USER': 'postgres',
-        'PASSWORD':  'postgres',
-        'HOST': 'picpay_db',
+        'NAME': config('POSTGRES_NAME'),
+        'USER': config('POSTGRES_USER'),
+        'PASSWORD':  config('POSTGRES_PASSWORD'),
+        'HOST': config('POSTGRES_HOST'),
         'PORT': '5432',
     },
     'dev': {
